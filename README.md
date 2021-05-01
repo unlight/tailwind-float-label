2a83d5ac66b127f5c13b1054416005214c73d802
Roman_Vasilev <s91630277@gmail.com>
2021-05-02 03:27:21 +0400

    feat: New helper class `.is-float-label-sticky`
    BREAKING CHANGE:
    Inversed markup for sticky label are not working anymore use `.is-float-label-sticky` instead

--- README.md
+++ README.md
@@ -42,10 +42,11 @@ Plugins adds three components supposed used together:
 -   `.has-float-label` Container which holds input control and label
 -   `.control-with-float-label` Form control
 -   `.is-float-label` Label
+-   `.is-float-label-sticky` Label which is always visible
 
 ```html
 <form class="max-w-xs mx-auto p-5 space-y-4">
-    <h2 class="text-2xl font-bold text-center">Login</h2>
+    <h2 class="text-2xl font-bold text-center">Example</h2>
     <div class="has-float-label">
         <input
             type="text"
@@ -54,26 +55,15 @@ Plugins adds three components supposed used together:
             placeholder="Name"
             class="control-with-float-label outline-none focus:shadow-outline border w-full px-1"
         />
-        <label for="name" class="is-float-label bg-white text-gray-700">Name</label>
-    </div>
-    <div class="has-float-label">
-        <label class="is-float-label bg-white text-gray-700">About</label>
-        <textarea
-            class="control-with-float-label outline-none focus:shadow-outline border w-full px-1"
-            placeholder="About"
-        >
-        </textarea>
+        <label for="name" class="is-float-label bg-white text-gray-500">Name</label>
     </div>
 </form>
 ```
 
 #### Notes:
 
-1. `.control-with-float-label` and `.is-float-label`
-   must be direct children of `.has-float-label`
-2. If label used before form control, label becomes sticky (always visible)
-3. If label used after form control, label is visible when control contains value
-   (classic float label pattern)
+`.control-with-float-label` and `.is-float-label`
+must be direct children of `.has-float-label`
 
 ## Similar Projects
 
@@ -83,3 +73,4 @@ Plugins adds three components supposed used together:
 
 -   https://tailwindow.github.io/component-tailwindcss/components/form/input-text/index.html
 -   https://github.com/you-dont-need/You-Dont-Need-JavaScript#Floating
+-   https://csslayout.io/patterns/floating-label/
--- src/index.ts
+++ src/index.ts
@@ -23,9 +23,9 @@ export default function floatLabelFactory(options?: Options) {
         {
             control: 'pt-1 pb-1',
             label: {
-                ['@apply leading-none px-1']: '',
-                fontSize: 'calc(42 / 60 * 1rem)',
-                top: 'calc(-22 / 60 * 1rem)',
+                ['@apply leading-none text-xs px-1']: '',
+                // fontSize: 'calc(42 / 60 * 100%)',
+                top: 0,
                 left: '0.25rem',
             },
         },
@@ -36,6 +36,17 @@ export default function floatLabelFactory(options?: Options) {
         const container = config.container ? toObject(config.container) : {};
         const control = config.control ? toObject(config.control) : {};
         const label = config.label ? toObject(config.label) : {};
+        const floatLabel = {
+            ...label,
+            // position: absolute; top/left will be faster
+            // if used on elements without transitions
+            position: 'absolute',
+            // transition: 'all 100ms',
+        };
+        const showLabel = {
+            opacity: 1,
+            transform: 'translate(0, -50%)',
+        };
 
         addComponents({
             '.has-float-label': {
@@ -44,22 +55,17 @@ export default function floatLabelFactory(options?: Options) {
                 display: 'flex',
                 flexDirection: 'column-reverse',
             },
-            '.is-float-label': {
-                ...label,
-                // position: absolute; top/left will be faster
-                // if used on elements without transitions
-                position: 'absolute',
-            },
+            '.is-float-label': floatLabel,
             '.control-with-float-label': {
                 ...control,
                 '& + .is-float-label': {
                     opacity: 0,
                 },
-                '&:not(:placeholder-shown)': {
-                    '& + .is-float-label': {
-                        opacity: 1,
-                    },
-                },
+                '&:not(:placeholder-shown) + .is-float-label': showLabel,
+            },
+            '.is-float-label-sticky': {
+                ...floatLabel,
+                ...showLabel,
             },
         });
     };
