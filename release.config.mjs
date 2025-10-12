6ebc0737496d917a3fca6fa19d284fcaad450ed9
roman <s91630277@gmail.com>
2025-10-12 21:57:28 +0400

    chore: Update other packages

---
+++ release.config.mjs
@@ -0,0 +1,28 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+export default {
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'conventionalcommits',
+      },
+    ],
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        preset: 'conventionalcommits',
+      },
+    ],
+    '@semantic-release/changelog',
+    [
+      '@semantic-release/npm',
+      {
+        pkgRoot: './dist',
+      },
+    ],
+    '@semantic-release/github',
+    '@semantic-release/git',
+  ],
+};
